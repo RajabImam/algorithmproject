e379adee2ce0b8371618bd3d9d27354804094f97
MajeurTest <chuzzle.9@hotmail.fr>
2022-06-05 13:44:17 +0200

    Correction Main test

--- src/main/java/ng/pencode/algorithmproject/Main.java
+++ src/main/java/ng/pencode/algorithmproject/Main.java
@@ -15,13 +15,15 @@ import java.util.Scanner;
  * @author RAJAB IMAM
  */
 public class Main {
-    
-     public static void main(String args[]) throws IOException{
+
+    public static void main(String args[]) throws IOException {
         Scanner input = new Scanner(System.in);
         // Create a unweighted-digraphs
         System.out.println("Introduce the path where the graph-DFS-BFS.txt file is stored");
         String file = input.nextLine();
-        if (file.isEmpty()) {file = "src/main/java/ng/pencode/algorithmproject/graph-DFS-BFS.txt";}
+        if (file.isEmpty()) {
+            file = "src/main/java/ng/pencode/algorithmproject/graph-DFS-BFS.txt";
+        }
 
         Graph diGraph = GraphFactory.createDiGraphFromTextFile(file);
         System.out.println(diGraph.getGraphType());
@@ -31,7 +33,9 @@ public class Main {
         // Create a weighted-digraphs
         System.out.println("Introduce the path where the graph-WDG.txt file is stored");
         String file2 = input.nextLine();
-        if (file2.isEmpty()) {file2 = "src/main/java/ng/pencode/algorithmproject/graph-WDG.txt";}
+        if (file2.isEmpty()) {
+            file2 = "src/main/java/ng/pencode/algorithmproject/graph-WDG.txt";
+        }
 
         Graph wdGraph = GraphFactory.createWDGraphFromTextFile(file2);
         System.out.println(wdGraph.getGraphType());
@@ -41,8 +45,8 @@ public class Main {
         System.out.println("Test the dfs(.) function with the graph is the graph-DFS-BFS.txt file");
         List<Integer> result = diGraph.dfs(5, new HashSet<Integer>());
         System.out.println("Visited order form vertex 5: ");
-        for (Integer vertex: result)
-            System.out.print(vertex+"->" );
+        for (Integer vertex : result)
+            System.out.print(vertex + "->");
         System.out.println("End");
 
         System.out.println(DijkstraSP.verifyNonNegative((WDgraph) wdGraph));
@@ -51,6 +55,6 @@ public class Main {
         System.out.println(dijkstraWDG.hasPathTo(v));
         System.out.println(dijkstraWDG.distTo(v));
         dijkstraWDG.printSP(v);
-     }
-    
+    }
+
 }
